8b5b7216d659a32f1ca7250a61658c8379039866
Jóhanna Svala Rafnsdóttir <johannasvala@Johannas-MBP.tskoli.is>
2018-10-09 14:02:50 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var hello = "hello" + " world!"; // hello er variable og hello world er string - mikilvægt að muna eftir bilinu á seinna orðinu, annars kemur bara helloworld í einu orði.
+/*
+ÞEtta er dæmi
+um rosalega 
+langt comment
+*/
+var tala = 5+9; //tala er variable sem inniheldur number
+var bool = true; // bool er variable sem inniheldur boolean
