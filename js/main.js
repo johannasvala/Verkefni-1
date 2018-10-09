e37011795436aaa058d76f4a8ff2731ca7e830c8
Jóhanna Svala Rafnsdóttir <johannasvala@Johannas-MBP.local>
2018-10-09 17:02:17 +0000

    mitt fyrsta verkefni - jóhanna

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-var hello = "hello" + " world!"; // hello er variable og hello world er string - mikilvægt að muna eftir bilinu á seinna orðinu, annars kemur bara helloworld í einu orði.
-/*
-ÞEtta er dæmi
-um rosalega 
-langt comment
-*/
-var tala = 5+9; //tala er variable sem inniheldur number
-var bool = true; // bool er variable sem inniheldur boolean
+// #1
+var strengur = "jóla"+"sveinn";
+// #2
+var tala = 5+5-2/2*5;
+// #3
+var boolean1 = (true&&true) || (!false);
+// #4
+var boolean2 = (4<5) === (5>4);
+// #5
+var boolean3 = (9<=12) !== (9>=10);
+// #6
+var svar6 = "af því að aboutKalli breytan segir að kalli sé góður strákur og === er jafnt og þannig að það er satt"
+// #7
+var svar7 = "breytan inniheldur að palli sé vondur strákur, fullyrðingin er sönn í rauninni"
+// #8
+var svar8 = "er svarið ekki bara að ! þýðir not, eða ekki og að tvö !! þýðir þá tvö ekki, má túlka sem mínus. Þess vegna er a false, b true, c false því strengur má ekki vera tómur og d er true... "
+// #9
+var svar9 = "true"
